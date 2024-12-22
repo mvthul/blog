8c8c8c1c60f1b4ede9b7bb8eb2fcb4d44c3b96a6
mvthul <michael@vthul-it.nl>
2024-12-22 15:48:38 +0000

    Delete UpgradeLiveEnvironment.ps1

--- UpgradeLiveEnvironment.ps1
+++
@@ -1,5 +0,0 @@
-docker build -t harbor.vthul-it.nl/private/blog:latest .
-docker push harbor.vthul-it.nl/private/blog:latest
-kubectl config use-context vthul-it
-kubectl apply -f .\kubernetes-deployment.yaml
-kubectl rollout restart deployment nextjs-blog -n nextjs
